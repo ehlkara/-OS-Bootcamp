7ed72072604629f4a0f5747fe98d1fa62559c0c7
ehlkara <ehlullah.fb@gmail.com>
2022-09-23 21:05:13 +0300

    create project

---
+++ Week1Day2/Week1Day2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week1Day2
+//
+//  Created by Ehlullah Karakurt on 23.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
